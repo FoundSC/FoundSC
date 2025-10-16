f7940626a246d6764df5dc147ba164e6444d6ecc
CALEBCH0 <gmcho03@gmail.com>
2025-10-15 17:30:44 -0700

    add leftover placeholder files

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,9 +1,9 @@
 // lib/supabse.js
-import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
+import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY;
+const SUPABSE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABSE_ANON_KEY);
 
